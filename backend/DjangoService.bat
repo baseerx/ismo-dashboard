07b6d6c4eeeb1afa657dcd7567878beb19895d4a
Muhammad Baseer Khan <baxeerkhan@gmail.com>
2026-07-29 14:59:40 +0500

    gitignore updated for staging

--- backend/DjangoService.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-set PYTHONPATH=%PYTHONPATH%;C:\Program Files\Apache24\htdocs\BioMetric-Django-Backend
-cd /d "C:\Program Files\Apache24\htdocs\BioMetric-Django-Backend"
-call "C:\Program Files\Apache24\htdocs\BioMetric-Django-Backend\venv\Scripts\activate.bat"
-start /b "" "C:\Program Files\Apache24\htdocs\BioMetric-Django-Backend\venv\Scripts\waitress-serve.exe" --host=0.0.0.0 --port=9002 hris.wsgi:application >> "C:\Program Files\Apache24\htdocs\BioMetric-Django-Backend\waitress.log" 2>&1
--- backend/run_django.vbs
+++
@@ -1,3 +0,0 @@
-Set WShell = CreateObject("WScript.Shell")
-WShell.Run """C:\Program Files\Apache24\htdocs\BioMetric-Django-Backend\DjangoService.bat""", 0
-Set WShell = Nothing
